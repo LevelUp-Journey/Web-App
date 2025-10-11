a48bfbd7d3d63fe1ce86a2f87c1d4c0f98274e92
JonatanFD <u20211b995@upc.edu.pe>
2025-10-11 17:35:09 -0500

    feat: add auth pages

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function SignInPage() {
+    return <div className="container mx-auto p-4 max-w-md"></div>;
+}
---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function SignUpPage() {
+    return <div className="container mx-auto p-4 max-w-md"></div>;
+}
