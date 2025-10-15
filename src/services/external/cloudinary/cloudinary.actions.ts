7ed5567aad075feba8d166c16220d7d6de7c163e
JonatanFD <u20211b995@upc.edu.pe>
2025-10-15 16:25:06 -0500

    feat: cloudinary service added, and drop zone component too

---
+++ src/services/external/cloudinary/cloudinary.actions.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import cloudinary from "./cloudinary.config";
+
+export async function cloudinaryUploadImage(image: string): Promise<string> {
+    try {
+        const result = await cloudinary.uploader.upload(image);s
+        return result.secure_url;
+    } catch (error) {
+        throw new Error('Failed to upload image');
+    }
+}
