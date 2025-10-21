d80f3205dac217c7e2fddc039d07bf7d966bed5f
JonatanFD <u20211b995@upc.edu.pe>
2025-10-21 00:54:00 -0500

    feat: ?

---
+++ src/app/(protected)/dashboard/(no-students)/admin/page.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { PATHS } from "@/lib/paths";
+import { ChallengeController } from "@/services/internal/challenges/controller/challenge.controller";
+import { AuthController } from "@/services/internal/iam/controller/auth.controller";
+
+export default async function AdministrativeDashboardPage() {
+    const userId = await AuthController.getUserId();
+    const challenges =
+        await ChallengeController.getChallengesByTeacherId(userId);
+    return (
+        <div className="container p-4 mx-auto">
+            {/* Welcome Message */}
+            <div className="mb-8">
+                <h1 className="text-3xl font-bold mb-2">
+                    Welcome to the Administrative Panel
+                </h1>
+                <p>Manage challenges and platform content from here.</p>
+            </div>
+
+            {/* Challenges Section */}
+            <div className="mb-6">
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="text-2xl font-semibold">Challenges</h2>
+                    <Link href={PATHS.DASHBOARD.CHALLENGES.CREATE}>
+                        <Button className="font-medium px-4 py-2 rounded-lg transition-colors duration-200">
+                            Create Challenge
+                        </Button>
+                    </Link>
+                </div>
+                <ul>
+                    {challenges.map((challenge) => (
+                        <li key={challenge.id}>
+                            <Link
+                                href={PATHS.DASHBOARD.CHALLENGES.EDIT(
+                                    challenge.id,
+                                )}
+                            >
+                                <Button className="font-medium px-4 py-2 rounded-lg transition-colors duration-200">
+                                    {challenge.name}
+                                </Button>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
