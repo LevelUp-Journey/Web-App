fc7b43d47963115a9af781e0061c0c1e1c15d7f6
JonatanFD <u20211b995@upc.edu.pe>
2025-11-09 17:43:35 -0500

    code format

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -13,9 +13,7 @@ export function ThemeToggle() {
             size="icon"
             onClick={(e) => {
                 e.preventDefault();
-                setTheme((prev) =>
-                    prev === "light" ? "dark" : "light",
-                );
+                setTheme((prev) => (prev === "light" ? "dark" : "light"));
             }}
         >
             <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
